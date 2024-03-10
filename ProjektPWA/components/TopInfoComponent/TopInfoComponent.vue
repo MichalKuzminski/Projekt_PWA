ff60cb9202d8460e077d981241788b2627cf1fb9
Patryk 46964 <kupferpatryk@gmail.com>
2024-03-10 12:15:53 +0100

    info-comonents&nuxt-ui

---
+++ ProjektPWA/components/TopInfoComponent/TopInfoComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="info-wrapper" v-if="!isClosed">
+    <div class="content-wrapper">
+        <slot></slot>
+        <Icon @click="closeHeader()" class="content-icon" name="ci:close-circle" color="white" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                isClosed: false
+            }
+        },
+        methods:{
+            closeHeader(): void{
+                this.isClosed = true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '@/globalStyles.scss';
+    .content-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        background-color: $header-info-background-color;
+        padding-inline: $header-info-padding;
+        height: $header-info-height;
+        font-size: 11px;
+        font-family: $global-font;
+        font-weight: 600;
+        gap: 3px;
+
+        .content-icon {
+            position: absolute;
+            right: $header-info-padding;;
+            transition: all 0.2s;
+            font-size: 22px;
+            &:hover {
+                transform: scale(1.07);
+            }
+        }
+    }
+</style>
