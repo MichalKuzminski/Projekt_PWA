891538f51daef8df1b450d1ad16b328a1dcf739e
Patrycja 75839 <24plewandowska@gmail.com>
2024-04-03 12:49:20 +0200

    pierwsza wersja

---
+++ ProjektPWA/components/Footer/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <div class="row mt-3">
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i> PORADNIK ZAKUPOWY
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Meotdy płatności</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Formy dostawy</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Reklamacje</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Zwrot, wymiana</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Kontakt</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Kontakt</a>
+            </p>
+          </div>
+
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">ZASADY I REGULAMINY</h6>
+            <p>
+              <a href="#!" class="text-reset">Regulamin sklepu</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset"
+                >Regulamin karty stałego klienta</a
+              >
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Regulamin karty podarunkowej</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Saldo karty podarunkowej</a>
+            </p>
+          </div>
+
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">O NAS</h6>
+            <p>
+              <a href="#!" class="text-reset">Regulamin sklepu</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset"
+                >Regulamin karty stałego klienta</a
+              >
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Regulamin karty podarunkowej</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Saldo karty podarunkowej</a>
+            </p>
+          </div>
+
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">IKONY</h6>
+            <i class="bi bi-facebook"></i>
+            <i class="bi bi-twitter-x"></i>
+            <i class="bi bi-instagram"></i>
+          </div>
+        </div>
+      </div>
+    </section>
+  </footer>
+</template>
