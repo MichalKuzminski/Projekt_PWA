2e8188c84c8e8857248821d45ac285da6d944b66
Patrycja 75839 <24plewandowska@gmail.com>
2024-04-05 21:24:56 +0200

    footer done

--- ProjektPWA/components/Footer/Footer.vue
+++ ProjektPWA/components/Footer/Footer.vue
@@ -1,78 +1,137 @@
 <template>
-  <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  <footer>
     <section class="">
-      <div class="container text-center text-md-start mt-5">
-        <div class="row mt-3">
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3"></i> PORADNIK ZAKUPOWY
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Meotdy płatności</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Formy dostawy</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Reklamacje</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Zwrot, wymiana</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Kontakt</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Kontakt</a>
-            </p>
-          </div>
+      <div class="grid-containter">
+        <div class="links-box">
+          <h3 class="links-box-title">Poradnik zakupowy</h3>
+          <ul>
+            <li class="links-box-link">
+              <a href="#!">Metody płatności</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Formy dostawy</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Reklamacje</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Zwrot, wymiana</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Kontakt</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Testowy Link</a>
+            </li>
+          </ul>
+        </div>
 
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold mb-4">ZASADY I REGULAMINY</h6>
-            <p>
-              <a href="#!" class="text-reset">Regulamin sklepu</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset"
-                >Regulamin karty stałego klienta</a
-              >
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Regulamin karty podarunkowej</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Saldo karty podarunkowej</a>
-            </p>
-          </div>
+        <div class="links-box">
+          <h3 class="links-box-title">Zasady i regulaminy</h3>
+          <ul>
+            <li class="links-box-link">
+              <a href="#!">Regulamin sklepu</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Regulamin karty stałego klienta</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Regulamin karty podarunkowej</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Saldo karty podarunkowej</a>
+            </li>
+          </ul>
+        </div>
 
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold mb-4">O NAS</h6>
-            <p>
-              <a href="#!" class="text-reset">Regulamin sklepu</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset"
-                >Regulamin karty stałego klienta</a
-              >
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Regulamin karty podarunkowej</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Saldo karty podarunkowej</a>
-            </p>
-          </div>
+        <div class="links-box">
+          <h3 class="links-box-title">O nas</h3>
+          <ul>
+            <li class="links-box-link">
+              <a href="#!">Regulamin sklepu</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Regulamin karty stałego klienta</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Regulamin karty podarunkowej</a>
+            </li>
+            <li class="links-box-link">
+              <a href="#!">Saldo karty podarunkowej</a>
+            </li>
+          </ul>
+        </div>
 
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <h6 class="text-uppercase fw-bold mb-4">IKONY</h6>
-            <i class="bi bi-facebook"></i>
-            <i class="bi bi-twitter-x"></i>
-            <i class="bi bi-instagram"></i>
-          </div>
+        <div class="links-box">
+          <Icon class="icon" name="jam:facebook-square" />
+
+          <Icon class="icon" name="devicon:twitter" />
+
+          <Icon class="icon" name="mdi:instagram" />
         </div>
       </div>
     </section>
   </footer>
 </template>
 
-<script lang="ts" setup></script>
+<script setup></script>
+
+<style lang="scss" scoped>
+@import '@/globalStyles.scss';
+
+.grid-containter {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  width: 1600px;
+}
+
+.links-box-title {
+  letter-spacing: 0.6px;
+  line-height: 1.5;
+  font-weight: 700;
+
+  text-transform: uppercase;
+  margin-bottom: 20px;
+  margin-top: 0;
+  text-align: left;
+}
+
+.links-box {
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 49px;
+  padding-left: 60px;
+  padding-right: 60px;
+}
+
+.links-box-link {
+  line-height: 1.9;
+  margin: 0 0 10px;
+  letter-spacing: 0.3px;
+  color: #000;
+}
+ul {
+  list-style: none;
+}
+
+.icon {
+  font-size: 30px;
+  margin-right: 20px;
+}
+
+footer {
+  box-sizing: border-box;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1600px;
+  padding-left: 40px;
+  padding-right: 40px;
+  width: auto;
+}
+
+section {
+  font-family: $global-font;
+  padding-top: 62px;
+  font-size: 12px;
+}
+</style>
